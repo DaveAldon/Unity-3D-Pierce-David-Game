5667f0d9060f7d00520045c3869ac35161f17402
Pierce Ortlieb <ortlieb1@gvsu.edu>
2016-06-07 16:56:29 -0400

    Added Collider Controller for cube to push rigidbody objects

--- Assets/Scripts/CubeScript.js
+++ Assets/Scripts/CubeScript.js
@@ -21,7 +21,25 @@ function Update () {
     }
     // Apply gravity
     moveDirection.y -= gravity * Time.deltaTime;
-
     // Move controller
     controller.Move(moveDirection * Time.deltaTime);
+} 
+
+    /* Pushing Component */
+var pushPower : float = 2.0;
+
+function OnControllerColliderHit(hit : ControllerColliderHit){
+    var body : Rigidbody = hit.collider.attachedRigidbody;
+    // When there is no rigidbody
+    if(body == null || body.isKinematic){
+    	return;
+    }
+    // Dont push object below the cube
+    if(hit.moveDirection.y < -0.3){
+    	return;
+    }
+    // Push objects side to side, not up/down
+    var pushDir : Vector3 = Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+    // Apply push
+    body.velocity = pushDir * pushPower;
 }
