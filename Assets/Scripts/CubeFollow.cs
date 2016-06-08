d4dce2a35012c32418ccd98001d950ae522617cb
David Crawford <crawford@mail.gvsu.edu>
2016-06-08 13:00:16 -0400

    Added new cube script that follows a target at a defined speed.

---
+++ Assets/Scripts/CubeFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeFollow : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed = 3;
+	public float rotationSpeed = 3;
+	public Transform myTransform;
+
+	public void Awake() {
+		myTransform = transform; //cache transform data for easy access/preformance
+	}
+
+	public void Start() {
+		target = GameObject.FindWithTag("Player").transform; //target the player, or target whatever object has the tag "Player"
+	}
+
+	public void Update () {
+		myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime); //rotate to look at the player
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime; //move towards the player
+	}
+}
