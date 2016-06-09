e2a2bd3161ce2248f3c97c6af2fae06990883536
David Crawford <crawford@mail.gvsu.edu>
2016-06-09 14:36:47 -0400

    Removed unused prefabs in scene 2. Also added a main menu that loads scene 2 through a button prompt.

--- Assets/Scripts/CubeFollow.cs
+++ Assets/Scripts/CubeFollow.cs
@@ -12,10 +12,6 @@ public class CubeFollow : MonoBehaviour {
 		myTransform = transform; //cache transform data for easy access/preformance
 	}
 
-	public void Start() {
-		target = GameObject.FindWithTag("Player").transform; //target the player, or target whatever object has the tag "Player"
-	}
-
 	public void Update () {
 		myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime); //rotate to look at the player
 		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime; //move towards the player
---
+++ Assets/Scripts/MainMenu Controllers/NewGameButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; //Resource needed to do scene management for loading scenes
+
+public class NewGameButton : MonoBehaviour {
+
+	public void SetActive (bool loadRequest) {
+		if (loadRequest == true) {
+			SceneManager.LoadScene ("2"); //Loads a scene with the given name as a String
+		}
+	}
+}
